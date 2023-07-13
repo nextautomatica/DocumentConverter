3811eeba7fbce729b0be46a70df39ce11c254f94
nextautomatica <134034444+nextautomatica@users.noreply.github.com>
2023-07-13 09:55:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
+from PyPDF2 import PdfFileReader
+import textract
+import csv
+import io
 
 app = FastAPI()
 
 class Msg(BaseModel):
     msg: str
 
+@app.post("/upload")
+async def upload_file(file: UploadFile = UploadFile(...)):
+    file_extension = file.filename.split(".")[-1]
+
+    if file_extension == 'pdf':
+        text = extract_text_from_pdf(file.file)
+    elif file_extension == 'csv':
+        text = extract_text_from_csv(file.file)
+    elif file_extension == 'doc':
+        text = extract_text_from_doc(file.file)
+    else:
+        return {"error": "Invalid file format. Please upload a PDF, CSV, or DOC file."}
+
+    return {"text": text}
+
+def extract_text_from_pdf(file):
+    pdf = PdfFileReader(file)
+    text = ""
+
+    for page_num in range(pdf.getNumPages()):
+        page = pdf.getPage(page_num)
+        text += page.extractText()
+
+    return text
+
+
+def extract_text_from_csv(file):
+    text = ""
+
+    # Assuming CSV file has a single column with text
+    reader = csv.reader(io.TextIOWrapper(file))
+    for row in reader:
+        text += ",".join(row)
+
+    return text
+
+
+def extract_text_from_doc(file):
+    text = textract.process(file)
+    return text.decode("utf-8")
 
 @app.get("/")
 async def root():
